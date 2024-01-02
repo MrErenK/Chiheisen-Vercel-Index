a43a8bf8f86fc3d5d3f190bb77666a7a8d8aa725
EreN <77717109+MrErenK@users.noreply.github.com>
2024-01-02 10:51:01 +0300

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -11,7 +11,7 @@ import { getOdAuthTokens, storeOdAuthTokens } from '../../utils/odAuthTokenStore
 import { runCorsMiddleware } from './raw'
 
 const basePath = pathPosix.resolve('/', siteConfig.baseDirectory)
-const clientSecret = revealObfuscatedToken(apiConfig.obfuscatedClientSecret)
+const clientSecret = apiConfig.obfuscatedClientSecret ? revealObfuscatedToken(apiConfig.obfuscatedClientSecret) : ''
 
 /**
  * Encode the path of the file relative to the base directory
